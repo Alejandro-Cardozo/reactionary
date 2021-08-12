708756a17396b1fdd2375f8bef599132c26bd6a5
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-08-12 15:34:52 -0300

    Styling fixes

--- src/components/Definitions/Definitions.jsx
+++ src/components/Definitions/Definitions.jsx
@@ -5,9 +5,18 @@ import classes from './Definitions.module.css';
 const Definitions = ({ word, category, meanings }) => {
   return (
     <div className={classes.meanings}>
+      {meanings[0] && word && category === 'en' && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ background: '#fff', borderRadius: 10 }}
+          controls
+        >
+          Your Browser doesn't support audio elements.
+        </audio>
+      )}
       {word === '' ? (
         <span className={classes.subtitle}>
-          Start by typing a word in Search
+          Start by typing a word in the search bar
         </span>
       ) : (
         meanings.map((mean) =>
